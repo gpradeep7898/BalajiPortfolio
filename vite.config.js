8360f8549655f0832962b0c0ecbccb2053510122
Pradeep Gatti <gpradeep7898@gmail.com>
2025-07-13 15:09:03 -0400

    Allow Render host in Vite preview config for deployment

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,12 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true
-  }
+  },
+  preview: {
+    allowedHosts: [
+      'balajiportfolio-0yri.onrender.com'
+    ],
+    host: true,
+    port: process.env.PORT || 10000,
+  },
 }) 
